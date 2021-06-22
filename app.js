e713406e6449dff80e5b6c9f864a8e38321ae5c2
ritamak <83585556+ritamak@users.noreply.github.com>
2021-06-22 18:55:11 +0100

    Rita & Enrique Lab

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + "/public"))
+
+app.get('/' , ( req, res ) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+app.get('/works' , ( req, res ) => {
+    res.sendFile(__dirname + '/views/works.html')
+})
+app.get('/about', (ihreq, ihres) => {
+    ihres.sendFile(__dirname + '/views/about.html')
+})
+app.get('/gallery', (ihreq, ihres) => {
+    ihres.sendFile(__dirname + '/views/gallery.html')
+})
+app.listen(3000)
